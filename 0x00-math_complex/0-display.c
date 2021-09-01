68898f4975304fb72d7a119d59ab0d84ddf7db78
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-09-01 19:11:55 +0000

    added task 0

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * display_complex_number - displays a complex numbers
+ * @c: complex umber to display.
+ */
+void display_complex_number(complex c)
+{
+
+/* print the real part */
+if (c.re)
+    printf("%.9g", c.re);
+
+/* print the imaginary part */
+if (c.im > 0)
+{
+    if (c.re)
+        printf(" + ");
+    if (c.im != 1)
+        printf("%.9gi\n", c.im);
+    else
+        printf("i\n");
+}
+else if (c.im < 0)
+{
+    if (c.re)
+        printf(" - ");
+    if (c.im != -1)
+        printf("%.9gi\n", c.im * (-1));
+    else
+        printf("i\n");
+}
+else
+    printf("\n");
+}
